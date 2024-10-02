fd61334ceb5456ce2c0ed6e12a156b6af780b5ac
bscit <54488066+tmaster-terrarian@users.noreply.github.com>
2024-10-02 15:21:11 -0700

    tweak tools styles

--- public/tools/toolgen.js
+++ public/tools/toolgen.js
@@ -2,13 +2,13 @@ const __tools = document.getElementById("tools")
 
 function genWeb(imageSrc, link, name, descriptionHtml)
 {
-    __tools.innerHTML += `
+    __tools.innerHTML += /*html*/`
     <div class="tool">
         <div class="preview">
             <img src="${imageSrc}" width="165">
         </div>
         <div class="info">
-            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3> <a href="${link}" target="_blank">online</a></span>
+            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3><p><a href="${link}" target="_blank">online</a></p></span>
             <hr>
             ${descriptionHtml}
         </div>
@@ -17,13 +17,13 @@ function genWeb(imageSrc, link, name, descriptionHtml)
 
 function genApp(imageSrc, link, name, descriptionHtml)
 {
-    __tools.innerHTML += `
+    __tools.innerHTML += /*html*/`
     <div class="tool">
         <div class="preview">
             <img src="${imageSrc}" width="165">
         </div>
         <div class="info">
-            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3> <a href="${link}" target="_blank">download</a></span>
+            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3><p><a href="${link}" target="_blank">download</a></p></span>
             <hr>
             ${descriptionHtml}
         </div>
@@ -32,13 +32,13 @@ function genApp(imageSrc, link, name, descriptionHtml)
 
 function genWebApp(imageSrc, linkWeb, linkLocal, name, descriptionHtml)
 {
-    __tools.innerHTML += `
+    __tools.innerHTML += /*html*/`
     <div class="tool">
         <div class="preview">
             <img src="${imageSrc}" width="165">
         </div>
         <div class="info">
-            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3> <a href="${linkWeb}" target="_blank">online</a><span style="cursor: default"> | </span><a href="${linkLocal}" target="_blank">download</a></span>
+            <span><h3 style="max-width: ${name.length}ch; display: inline-block">${name}</h3><p><a href="${linkWeb}" target="_blank">online</a><span style="cursor: default"> | </span><a href="${linkLocal}" target="_blank">download</a></p></span>
             <hr>
             ${descriptionHtml}
         </div>
